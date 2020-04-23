738c01545d3a47911c09538b6c398222ed75fb5c
Shiro <khoirul.20998@gmail.com>
2020-04-23 12:39:18 +0700

    Uploads

---
+++ ios/Pods/Target Support Files/Pods-baca_buku_mobile-tvOS/Pods-baca_buku_mobile-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_baca_buku_mobile_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_baca_buku_mobile_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-baca_buku_mobile-tvOSTests/Pods-baca_buku_mobile-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_baca_buku_mobile_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_baca_buku_mobile_tvOSTests
+@end
